4986bf097224e8b1a7b3b443cc77014d4d9cf019
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-16 21:03:07 +0530

    Impl Box for Dinf - skip children parsing

--- lib/parsing/dinf.ex
+++ lib/parsing/dinf.ex
@@ -1,3 +1,13 @@
 defmodule Dinf do
-  defstruct(data: nil)
+  defstruct(
+    name: :dinf,
+    data: nil
+  )
+end
+
+defimpl Box, for: Dinf do
+  def parse(_, file, size) do
+    :file.position(file, {:cur, size})
+    %Dinf{}
+  end
 end
