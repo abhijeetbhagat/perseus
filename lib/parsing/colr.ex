75c295789e8c4057cc9cc7c9ef00adf75814ca82
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-17 21:27:58 +0530

    Add skeletal btrt, colr and pasp boxes

---
+++ lib/parsing/colr.ex
@@ -0,0 +1,13 @@
+defmodule Colr do
+  defstruct(
+    name: :colr,
+    data: nil
+  )
+end
+
+defimpl Box, for: Colr do
+  def parse(_, file, size) do
+    :file.position(file, {:cur, size})
+    %Colr{}
+  end
+end
