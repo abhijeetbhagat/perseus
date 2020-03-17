8e4fd41e45798b384c4254d8c0543cf49de8e9e4
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-17 23:40:48 +0530

    Fix stsd parsing - read version, next box header

--- lib/parsing/stsd.ex
+++ lib/parsing/stsd.ex
@@ -1,6 +1,54 @@
 defmodule Stsd do
   defstruct(
     name: :stsd,
-    data: nil
+    entry_count: 0,
+    avc1: nil,
+    mp4a: nil
   )
+
+  defmodule Loop do
+    def loop(<<length::integer-32, atom_type::binary-4>>, file, cnt, size, stsd)
+        when cnt < size do
+      IO.puts("stsd: cnt: #{cnt}, size: #{size}, length: #{length}")
+
+      box =
+        case atom_type do
+          "avc1" ->
+            %Avc1{}
+
+          "mp4a" ->
+            %Mp4a{}
+
+          type ->
+            IO.puts("Invalid atom type #{type} found during parsing")
+            throw(atom_type)
+        end
+
+      box = Box.parse(box, file, length - 8)
+      IO.puts(inspect(box))
+
+      loop(IO.binread(file, 8), file, cnt + length, size, stsd |> Map.put(box.name, box))
+    end
+
+    def loop(:eof, _, _, _, stsd) do
+      stsd
+    end
+
+    def loop(<<_::integer-32, _::binary-4>>, file, _, _, stsd) do
+      # because the current pos is past next atom's length and name
+      :file.position(file, {:cur, -8})
+      stsd
+    end
+
+    def loop({:error, reason}, _, _, _, _) do
+      IO.puts("Error occurred while reading file #{reason}")
+    end
+  end
+end
+
+defimpl Box, for: Stsd do
+  def parse(_, file, size) do
+    <<_version::integer-32, entry_count::integer-32>> = IO.binread(file, 8)
+    Stsd.Loop.loop(IO.binread(file, 8), file, 0, size - 4, %Stsd{entry_count: entry_count})
+  end
 end
