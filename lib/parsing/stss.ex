4379a880b1961d9a75ad5bba871fd761f167669c
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-18 11:36:04 +0530

    Add stts, stss parsing

--- lib/parsing/stss.ex
+++ lib/parsing/stss.ex
@@ -1,6 +1,35 @@
 defmodule Stss do
   defstruct(
     name: :stss,
-    data: nil
+    entry_count: 0,
+    sample_number: []
   )
+
+  defmodule Loop do
+    def loop(<<sn::integer-32, rest::binary>>, cnt, size, sn_l)
+        when cnt < size do
+      loop(rest, cnt + 1, size, sn_l ++ [sn])
+    end
+
+    def loop(_rest, _, _, sn_l) do
+      sn_l
+    end
+  end
+end
+
+defimpl Box, for: Stss do
+  def parse(_, file, size) do
+    <<
+      _version::integer-32,
+      entry_count::integer-32,
+      rest::binary
+    >> = IO.binread(file, size)
+
+    sn_l = Stss.Loop.loop(rest, 0, entry_count, [])
+
+    %Stss{
+      entry_count: entry_count,
+      sample_number: sn_l
+    }
+  end
 end
--- lib/parsing/stts.ex
+++ lib/parsing/stts.ex
@@ -1,6 +1,37 @@
 defmodule Stts do
   defstruct(
     name: :stts,
-    data: nil
+    entry_count: 0,
+    sample_count: [],
+    sample_delta: []
   )
+
+  defmodule Loop do
+    def loop(<<sc::integer-32, sd::integer-32, rest::binary>>, cnt, size, sc_l, sd_l)
+        when cnt < size do
+      loop(rest, cnt + 1, size, sc_l ++ [sc], sd_l ++ [sd])
+    end
+
+    def loop(_rest, _, _, sc_l, sd_l) do
+      {sc_l, sd_l}
+    end
+  end
+end
+
+defimpl Box, for: Stts do
+  def parse(_, file, size) do
+    <<
+      _version::integer-32,
+      entry_count::integer-32,
+      rest::binary
+    >> = IO.binread(file, size)
+
+    {sc_l, sd_l} = Stts.Loop.loop(rest, 0, entry_count, [], [])
+
+    %Stts{
+      entry_count: entry_count,
+      sample_count: sc_l,
+      sample_delta: sd_l
+    }
+  end
 end
