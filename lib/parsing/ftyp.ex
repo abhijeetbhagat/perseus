2db9201608c8df3d33ee68bca4b0eab80037d7e2
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-19 23:32:12 +0530

    Refactor ftyp parsing - use comprehension

--- lib/parsing/ftyp.ex
+++ lib/parsing/ftyp.ex
@@ -1,27 +1,19 @@
 require Logger
+
 defmodule FTyp do
   defstruct(
     major_brand: "",
     minor_version: 0,
     compatible_brands: []
   )
-
-  defmodule Loop do
-    def loop(_file, l, 0) do
-      l
-    end
-
-    def loop(file, l, i) do
-      <<compatible_brand::binary-4>> = IO.binread(file, 4)
-      loop(file, [compatible_brand | l], i - 4)
-    end
-  end
 end
 
 defimpl Box, for: FTyp do
   def parse(_, file, size) do
-    <<major_brand::binary-4, minor_version::integer-32>> = IO.binread(file, 8)
-    brands = FTyp.Loop.loop(file, [], size - 16)
+    <<major_brand::binary-4, minor_version::integer-32, rest::binary>> =
+      IO.binread(file, size - 8)
+
+    brands = for <<cb::binary-4 <- rest>>, do: cb
     %FTyp{major_brand: major_brand, minor_version: minor_version, compatible_brands: brands}
   end
 end
