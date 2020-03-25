f3ac2e49fab8eae43ef28801b54b77007b136dfd
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-03-25 15:30:22 +0530

    Parse a few additional AVCC info

--- lib/perseus/boxes/avcc.ex
+++ lib/perseus/boxes/avcc.ex
@@ -52,20 +52,44 @@ defimpl Perseus.Box, for: Avcc do
     num_sps = num_sps &&& 0x1F
     {rest, sps_len_l, sps_nalu_l} = loop(rest, [], [], 0, num_sps)
     <<num_pps::integer-8, rest::binary>> = rest
-    {_rest, pps_len_l, pps_nalu_l} = loop(rest, [], [], 0, num_pps)
+    {rest, pps_len_l, pps_nalu_l} = loop(rest, [], [], 0, num_pps)
 
-    %Avcc{
-      config_version: config_version,
-      profile_indication: profile_indication,
-      profile_compatibility: profile_compatibility,
-      level_indication: level_indication,
-      len_size_minus_one: len_size_minus_one,
-      num_sps: num_sps,
-      sps_len: sps_len_l,
-      sps_nalus: sps_nalu_l,
-      num_pps: num_pps,
-      pps_len: pps_len_l,
-      pps_nalus: pps_nalu_l
-    }
+    avcc =
+      case profile_indication == 100 or
+             profile_indication == 110 or
+             profile_indication == 122 or
+             profile_indication == 144 do
+        n when n in [100, 110, 122, 144] ->
+          <<
+            _::size(6),
+            chroma_format::integer-2,
+            _::size(5),
+            bit_depth_luma_minus8::integer-3,
+            _::size(5),
+            bit_depth_chroma_minus8::integer-3,
+            _rest::binary
+          >> = rest
+
+          %Avcc{}
+          |> Map.put(:chroma_format, chroma_format)
+          |> Map.put(:bit_depth_luma_minus8, bit_depth_luma_minus8)
+          |> Map.put(:bit_depth_chroma_minus8, bit_depth_chroma_minus8)
+
+        _ ->
+          %Avcc{}
+      end
+
+    avcc
+    |> Map.put(:config_version, config_version)
+    |> Map.put(:profile_indication, profile_indication)
+    |> Map.put(:profile_compatibility, profile_compatibility)
+    |> Map.put(:level_indication, level_indication)
+    |> Map.put(:len_size_minus_one, len_size_minus_one)
+    |> Map.put(:num_sps, num_sps)
+    |> Map.put(:sps_len, sps_len_l)
+    |> Map.put(:sps_nalus, sps_nalu_l)
+    |> Map.put(:num_pps, num_pps)
+    |> Map.put(:pps_len, pps_len_l)
+    |> Map.put(:pps_nalus, pps_nalu_l)
   end
 end
